06e5f12e493c72eda320733f71f91c380a247e68
Nils Martel <nilsmartel@yahoo.de>
2022-09-20 21:33:31 +0200

    Include logic to execute sample query

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use anyhow::Result;
+use postgres::{fallible_iterator::FallibleIterator, Client, NoTls};
+
+/// returns (User, Password, Database)
+fn get_credentials() -> Result<(String, String, String)> {
+    use std::env::var;
+
+    Ok((
+        var("DATABASE_USER")?,
+        var("DATABASE_PASSWORD")?,
+        var("DATABASE_DB")?,
+    ))
+}
+
+fn get_config_str() -> Result<String> {
+    // example string:
+    //  "postgresql://dboperator:operatorpass123@localhost:5243/postgres"
+
+    let (user, password, database) = get_credentials()?;
+
+    Ok(format!(
+        "postgresql://{user}:{password}@localhost:5243/{database}"
+    ))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = {
+        let s = get_config_str().expect("to read credentials for database");
+        Client::connect(&s, NoTls).expect("to make postgres connection")
+    };
+
+    let query = "SELECT * FROM gittables_tables_info";
+    for row in client.query(query, &[]).unwrap() {
+        println!("{:#?}", row);
+    }
 }
