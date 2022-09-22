df17796981a5365f7a16e0e766a68654ce579b48
Nils Martel <nilsmartel@yahoo.de>
2022-09-22 12:19:58 +0200

    Fix code
    by removing port from connection string

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,29 @@ use anyhow::Result;
 use postgres::{Client, NoTls};
 
 /// returns (User, Password, Database)
-fn get_credentials() -> Result<[String; 4]> {
+fn get_credentials() -> Result<[String; 3]> {
     use std::env::var;
 
     Ok([
         var("DATABASE_USER")?,
         var("DATABASE_PASSWORD")?,
         var("DATABASE_DB")?,
-        var("DATABASE_PORT").unwrap_or("5243".to_string()),
     ])
 }
 
 // example string:
 //  "postgresql://dboperator:operatorpass123@localhost:5243/postgres"
-fn get_config_str([user, password, database, port]: [String; 4]) -> String {
-    format!("postgresql://{user}:{password}@localhost:{port}/{database}")
+fn get_config_str([user, password, database]: [String; 3]) -> String {
+    format!("postgresql://{user}:{password}@localhost/{database}")
 }
 
 fn main() {
     let mut client = {
-        let s = "postgresql://postgres:postgres@localhost/library";
-        // let s = get_credentials()
-        //     .map(get_config_str)
-        //     .expect("to read credentials for database");
+        let s = get_credentials()
+            .map(get_config_str)
+            .expect("to read credentials for database");
 
-        match Client::connect(s, NoTls) {
+        match Client::connect(&s, NoTls) {
             Ok(c) => c,
             Err(e) => {
                 eprintln!("failed to connect to database: {e}");
@@ -35,7 +33,7 @@ fn main() {
         }
     };
 
-    let query = "SELECT * FROM gittables_tables_info";
+    let query = "SELECT * FROM gittables_tables_info LIMIT 10";
     for row in client.query(query, &[]).unwrap() {
         println!("{:#?}", row);
     }
